227507436a90db2a04c1b5182aacbee39659caf7
Steve Larsen <slarsen-3@att.net>
2021-03-22 18:51:19 -0500

    JSON parse training

---
+++ script.js
@@ -0,0 +1,28 @@
+// const str = `{"data": [{ "shoppingCart": "Tenzo tea", "items": ["tea", "whisk"]}]}`;
+
+// const obj = {data: [{ shoppingCart: "Tenzo tea", "items": ["tea", "whisk"]}]};
+
+// // console.log(strdata[0].shoppingCart);
+
+// console.log(obj.data[0].shoppingCart)
+
+// console.log(JSON.parse(str).data[0].items)
+
+// $.get("/Code_X/Level2/getJSON.json").done((data) => {
+//   console.log(typeof data);
+//   console.log(data);
+//   const dataToString = JSON.stringify(data);
+//   console.log(typeof dataToString);
+//   console.log(dataToString);
+// });
+
+// fetch("/Code_X/Level2/getJSON.json")
+//   .then((data) => data.json())
+//   .then((data) => {
+//     console.log(data);
+//     console.log(data.name);
+//     console.log(data.languages);
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   })
