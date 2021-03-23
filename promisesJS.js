14146d1add6b0c42671cf4a3e87fd636ee25195e
Steve Larsen <slarsen-3@att.net>
2021-03-23 16:22:00 -0500

    promisesJS

---
+++ promisesJS.js
@@ -0,0 +1,56 @@
+let p = new Promise((resolve, reject) => {
+  let a = 1 + 2;
+  if (a == 2) {
+    resolve("Success");
+  } else {
+    reject("Failed");
+  }
+});
+
+p.then((message) => {
+  console.log(message);
+}).catch((message) => {
+  console.log(message);
+});
+
+function makeRequest(location) {
+  return new Promise((resolve, reject) => {
+    console.log(`Making a request to ${location}`);
+    if (location === "Google") {
+      resolve("Google says hi");
+    } else {
+      reject("We can only talk to Google");
+    }
+  });
+}
+
+function processRequest(response) {
+  return new Promise((resolve, reject) => {
+    console.log("Processing response");
+    resolve(`Extra Information + ${response}`);
+  });
+}
+
+// makeRequest("Google")
+//   .then((response) => {
+//     console.log("Response received");
+//     return processRequest(response);
+//   })
+//   .then((processedResponse) => {
+//     console.log(processedResponse);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+async function doWork() {
+  try {
+    const response = await makeRequest("Google");
+    console.log("Response received");
+    const processedResponse = await processRequest(response);
+    console.log(processedResponse);
+  } catch (error) {
+    console.log(error);
+  }
+}
+doWork();
