5759907a35d1ae1b11f3f1628a4cb4dbf0dea6ed
Steve Larsen <slarsen-3@att.net>
2021-03-24 15:25:28 -0500

    jQuery training

--- script.js
+++ script.js
@@ -26,3 +26,32 @@
 //   .catch(error => {
 //     console.log(error);
 //   })
+let listElements;
+listElements = $("li").hasClass("nav-item");
+console.log(listElements);
+
+let active;
+active = $(".active");
+active.toggleClass("active").next().css("background-color", "orange");
+
+var navList, firstItem, link;
+navList = $(".nav-list");
+firstItem = navList.children().first();
+link = firstItem.find("a");
+link.attr("href", "#1");
+
+console.log(navList.text());
+// active.remove()
+$(".input-test").on("keypress", function () {
+  // console.log("The event happened");
+  $("body").css("background-color", "aqua");
+});
+
+$("#my-button").on("click", function () {
+  $("button").remove();
+  $("body").addClass("success");
+});
+
+$("#my-list").on("click", "li", function () {
+  $("li").css("background-color", "yellow");
+});
