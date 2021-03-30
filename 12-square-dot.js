f7dbaf0ed8a248e653d139f4b49fb88b754551df
Steve Larsen <slarsen-3@att.net>
2021-03-30 17:09:45 -0500

    square dot notation

---
+++ 12-square-dot.js
@@ -0,0 +1,14 @@
+// Differences between dot notation and square bracket syntax
+
+let sam = {
+  castiel: "mary",
+};
+
+let dean = {
+  john: "crowley",
+  mary: "chuck",
+};
+
+console.log(sam.castiel)
+console.log(dean[sam.castiel]); 
+console.log(dean.john)
